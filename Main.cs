27b5c8cdf006267a5492f1c65102f4b8d5657f67
msh31 <mshendriks03@gmail.com>
2025-09-17 23:17:43 +0200

    feat: complete game!
    - add colorRect for background color / win indicator
    - add textureRect for showing the A.I.'s move to the player
    - add seperate update UI method like the casino game
    - improve logic
    - make game's default size bigger (1600x900)

--- Main.cs
+++ Main.cs
@@ -1,46 +1,68 @@
 using Godot;
-using System;
 
-public partial class Main : Godot.Node2D 
+public partial class Main : Godot.Node2D
 {
-    enum Moves { 
-        ROCK = 0, 
-        PAPER = 1, 
-        SCISSORS = 2
-    };
-    
-    private int randomNumber;
-    
-    public override void _Ready()  {
-        
-    }
-    
-    public void _on_rock_pressed() {
-        battle(Moves.ROCK);
-    }
+	enum Moves
+	{
+		ROCK = 0,
+		PAPER = 1,
+		SCISSORS = 2
+	};
 
-    public void _on_paper_pressed() {
-        battle(Moves.PAPER);
-    }
+	// assign nodes directly from the editor
+	[Export] public ColorRect BackgroundNode;
+	[Export] public TextureRect AIResult;
+	
+	private Texture2D rockTexture;
+	private Texture2D paperTexture;
+	private Texture2D scissorsTexture;
 
-    public void _on_scissor_pressed() {
-        battle(Moves.SCISSORS);
-    }
+	public override void _Ready() {
+		rockTexture = GD.Load<Texture2D>("res://rock.png");
+		paperTexture = GD.Load<Texture2D>("res://paper.png");
+		scissorsTexture = GD.Load<Texture2D>("res://scissors.png");
+	}
 
-    private void battle(Moves playerMove) {
-        Moves aiMove = (Moves)(GD.Randi() % 3);
-        
-        GD.Print($"You chose: {playerMove}");
-        GD.Print($"AI chose: {aiMove}");
-        
-        if (playerMove == aiMove) {
-            GD.Print("It's a tie!");
-        } else if ((playerMove == Moves.ROCK && aiMove == Moves.SCISSORS) ||
-                (playerMove == Moves.PAPER && aiMove == Moves.ROCK) ||
-                (playerMove == Moves.SCISSORS && aiMove == Moves.PAPER)) {
-            GD.Print("You win!");
-        } else {
-            GD.Print("You lose!");
-        }
-    }
+
+	public void _on_rock_pressed() {
+		battle(Moves.ROCK);
+	}
+
+	public void _on_paper_pressed() {
+		battle(Moves.PAPER);
+	}
+
+	public void _on_scissor_pressed() {
+		battle(Moves.SCISSORS);
+	}
+
+	private void battle(Moves playerMove) {
+		Moves aiMove = (Moves)(GD.Randi() % 3);
+
+		if (playerMove == aiMove) {
+			UpdateUI(Colors.Gray, aiMove);
+		} else if ((playerMove == Moves.ROCK && aiMove == Moves.SCISSORS) ||
+				 (playerMove == Moves.PAPER && aiMove == Moves.ROCK) ||
+				 (playerMove == Moves.SCISSORS && aiMove == Moves.PAPER)) {
+			UpdateUI(Colors.Green, aiMove);
+		} else {
+			UpdateUI(Colors.Red, aiMove);
+		}
+	}
+
+	private void UpdateUI(Color color, Moves aiMove) {
+		if (BackgroundNode != null && AIResult != null) {
+			BackgroundNode.Color = color;
+			
+			if (aiMove == Moves.ROCK) {
+				AIResult.Texture = rockTexture;
+			}
+			else if (aiMove == Moves.PAPER) 	{
+				AIResult.Texture = paperTexture;
+			}
+			else {
+				AIResult.Texture = scissorsTexture;
+			}
+		}
+	}
 }
